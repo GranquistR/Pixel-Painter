27218f1df34a8b9c5f92a8dd57552f5c40802ee7
Riley Dummer <dummerr2357@my.uwstout.edu>
2024-09-25 15:20:54 -0500

    Base line for ArtController

---
+++ MyTestVueApp.Server/Controllers/ArtControllers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTestVueApp.Server.Entities;
+using MyTestVueApp.Server.Interfaces;
+
+namespace MyTestVueApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArtControllers
+    {
+        private ILogger<ArtControllers> ArtLog { get; }
+        
+        
+    }
+}
