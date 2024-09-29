931843f450e0688ffe041dd259749c88875bf5d6
Riley Dummer <dummerr2357@my.uwstout.edu>
2024-09-29 15:57:31 -0500

    Adding getters and setters for ArtPainting

--- MyTestVueApp.Server/Entities/ArtPainting.cs
+++ MyTestVueApp.Server/Entities/ArtPainting.cs
@@ -2,6 +2,14 @@
 {
     public class ArtPainting
     {
+        public int ArtId { get; set; }
+        public string ArtName { get; set; }
+
+        public int ArtistId { get; set; }
+
+        public int ArtWidth { get; set; }
+
+        public int ArtHeight { get; set; }
 
     }
 }
