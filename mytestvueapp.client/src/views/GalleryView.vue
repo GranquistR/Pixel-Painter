57c80ac96516c152434ef457dde891c98e436f6c
JacobSletten <slettenj2628@my.uwstout.edu>
2024-10-02 15:57:11 -0500

    Removed Dummy Data

--- mytestvueapp.client/src/views/GalleryView.vue
+++ mytestvueapp.client/src/views/GalleryView.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
     import { onMounted, ref } from "vue";
-    import artworkData from "../dummydata/DummyArt"
     import ArtCard from "../components/ArtCard.vue"
     import Art from "@/entities/Art";
     import ArtAccessService from "@/utils/ArtAccessService";
 
-    const artworks = ref(artworkData) // Dummy Data
     const stuff = ref<Art[] | null>(null); // Local call
-    const stuff2 = ref<Art[] | null>(null); // Service call
+    const stuff2 = ref<IterableIterator<Art> | null>(null); // Service call
 
     //ArtAccessService.getAllArt().then((r) => stuff2.value = r);
 
     onMounted(() => {
         fetchData();
-        //ArtAccessService.getAllArt().then((promise) => stuff2.value = promise as Art[]);
+        //ArtAccessService.getAllArt().then((promise) => stuff2.value = promise.values);
     });
 
     function fetchData() {
@@ -33,7 +31,6 @@
 <template>
     <div class="w-9 mx-auto my-0">
         <div class="flex flex-wrap">
-            <ArtCard v-for="art in artworks" :key="art.artId" :art="art"/> 
             <ArtCard v-for="art in stuff" :key="art.artId" :art="art" />
             <ArtCard v-for="art in stuff2" :key="art.artId" :art="art" />
         </div>
