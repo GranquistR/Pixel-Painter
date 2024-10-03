76ae3f93ab3146e7c9f3720cc038e88176ec010f
JohnNStout <100367310+JohnNStout@users.noreply.github.com>
2024-10-03 16:13:59 -0500

    Decode done
    Finished Decode and added some some text areas to display encoded string. and then decode that string.

--- mytestvueapp.client/src/utils/codec.ts
+++ mytestvueapp.client/src/utils/codec.ts
@@ -2,7 +2,7 @@ import { PixelGrid } from "@/entities/PixelGrid";
 
 export default class codec{
   
-public static Encode(inputGrid: PixelGrid): void{
+public static Encode(inputGrid: PixelGrid): string{
     let EncodedPicture: string ="";
 
     for (let i = 0; i < inputGrid.height; i++) {
@@ -12,24 +12,24 @@ public static Encode(inputGrid: PixelGrid): void{
       }
     }
     console.log(EncodedPicture);
+    return(EncodedPicture);
   }
 
   public static Decode(encodedString: String, height: number, width: number): PixelGrid{
     const decodedPicture: PixelGrid = new PixelGrid(height, width);
+    decodedPicture.createGrid(height, width);
+    let k = 0;
 
     if (encodedString === "") {
-        decodedPicture.createGrid(height, width);
         return decodedPicture;
     }
-
-    decodedPicture.createGrid(height, width);
-
     for (let i = 0; i < height; i++) {
       for (let j = 0; j < width; j++) {
-        decodedPicture.grid[i][j]="#" + encodedString.slice(0,6);
-        encodedString.substring(6);
+        decodedPicture.grid[i][j]="#" + encodedString.slice(k,k+6);
+         k= k+6;
       }
     }
+    
     return decodedPicture;
   }
 }
--- mytestvueapp.client/src/views/PainterView.vue
+++ mytestvueapp.client/src/views/PainterView.vue
@@ -8,23 +8,48 @@
       @click="pixelGrid.randomizeGrid()"
     />
     <Button label="Recenter" class="mr-2" @click="canvas.recenter()" />
-    <Button label="Encode painting" class="mr-2" @click="codec.Encode(pixelGrid)" />
-    <Button label="Decode Painting" class="mr-2" @click="codec.Decode('',16,16)" />
+    <Button label="Encode painting" class="mr-2" @click="printEncodedText" />
+    <Button label="Decode Painting" class="mr-2" @click="decodeToCanvas" />
     
-
+    
+    <div class="mr-2"style="height:100px;width:140px;overflow:auto;word-wrap: break-word;padding:10px;">{{stringEncodedText}}</div>
+    
+    <Textarea class="mr-2" name ='textArea' style="height:100px;width:140px;" placeholder ="Copy and paste encoded text here and press decode."/>
+    
+    
+    
   </div>
+ 
+  
+    
+  
 </template>
 
 <script setup lang="ts">
 import DrawingCanvas from "@/components/DrawingCanvas.vue";
 import { PixelGrid } from "@/entities/PixelGrid";
 import codec from "@/utils/codec";
-import { ref } from "vue";
+import { ref, Text } from "vue";
 import Button from "primevue/button";
-
+import Textarea from 'primevue/textarea';
 
 const pixelGrid = ref<PixelGrid>(new PixelGrid(64, 64));
 pixelGrid.value.randomizeGrid();
+
+let stringEncodedText= ref('Encoded text will show here.');
+
+const printEncodedText = () => {
+  stringEncodedText.value = codec.Encode(pixelGrid.value);
+}
+
+const decodeToCanvas = () => {
+  console.log(stringEncodedText.value);
+  let decodedPixelGrid = <PixelGrid>(new PixelGrid(64, 64));
+  decodedPixelGrid=codec.Decode(stringEncodedText.value,64,64);
+  pixelGrid.value.updateGrid(decodedPixelGrid);
+ 
+}
+
 
 const canvas = ref();
 </script>
