33dcbdf59866952905c670e548ee3416717c0754
Riley Dummer <dummerr2357@my.uwstout.edu>
2024-10-03 09:46:52 -0500

    Fixed the entry point file to connect to the database and fixed the GetArtById function

--- MyTestVueApp.Server/ServiceImplementations/ArtAccessService.cs
+++ MyTestVueApp.Server/ServiceImplementations/ArtAccessService.cs
@@ -53,7 +53,35 @@ namespace MyTestVueApp.Server.ServiceImplementations
 
         public Art GetArtById(int id)
         {
-            return new Art();
+            var connectionString = AppConfig.Value.ConnectionString;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                //var query = "SELECT Date, TemperatureC, Summary FROM WeatherForecasts";
+                var query = "SELECT ID, ArtName, Artistid, Width, ArtLength, Encode, CreationDate, isPublic FROM Art WHERE ID=" + id;
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read() != null)
+                        {
+                            var painting = new Art
+                            { //ArtId, ArtName, ArtistId, Width, ArtLength, Encode, Date, IsPublic
+                                ArtId = reader.GetInt32(0),
+                                ArtName = reader.GetString(1),
+                                ArtistId = reader.GetInt32(2),
+                                Width = reader.GetInt32(3),
+                                ArtLength = reader.GetInt32(4),
+                                Encode = reader.GetString(5),
+                                CreationDate = reader.GetDateTime(6),
+                                IsPublic = reader.GetBoolean(7)
+                            };
+                            return painting;
+                        }
+                        return null;
+                    }
+                }
+            }
         }
     }
 }
