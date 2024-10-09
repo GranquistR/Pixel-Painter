1b40e3d7aba85d3abb41064fd2facea8cf391cdb
AngdroRyu <cookmaster22@yahoo.com>
2024-10-09 15:38:31 -0500

    got the Viewer running (hardcoded) updated database to have more entries

---
+++ mytestvueapp.client/src/components/MyCanvas/MyCanvas.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="my-canvas-wrapper">
+      <canvas ref="my-canvas"></canvas>
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        provider: {
+          context: null,
+        },
+      };
+    },
+  
+    provide() {
+      return {
+        provider: this.provider,
+      };
+    },
+  
+    mounted() {
+      this.provider.context = this.$refs['my-canvas'].getContext('2d');
+  
+        this.$refs['my-canvas'].width = this.$refs['my-canvas'].parentElement.clientWidth;
+            this.$refs['my-canvas'].height = 350;
+      //      this.$refs['my-canvas'].parentElement.clientHeight;
+    },
+  };
+  </script>
---
+++ mytestvueapp.client/src/views/ImageViewer.vue
@@ -0,0 +1,21 @@
+
+<template>
+
+    <div class="p-8">
+        <my-canvas >
+            <MyBox>
+
+            </MyBox>
+        </my-canvas>
+    </div>
+    <div class = "border-solid border-1" >
+        <h2>"this is some text"</h2>
+    </div>
+</template>
+<script setup lang="ts">
+    import MyCanvas from '@/components/MyCanvas/MyCanvas.vue';
+    import MyBox from '@/components/MyCanvas/MyBox.vue';
+    import Button from "primevue/button";
+
+</script>
+
