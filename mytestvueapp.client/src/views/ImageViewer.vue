56f90714d0cde9b1c3393a792bb2a521684caba4
AngdroRyu <cookmaster22@yahoo.com>
2024-10-14 16:14:58 -0500

    fixed typo in init, access endpoint inside of imageviewer, and uses prop in Mycanvas

--- mytestvueapp.client/src/views/ImageViewer.vue
+++ mytestvueapp.client/src/views/ImageViewer.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="p-8 justify-content-center flex w-full h-full align-items-center">
     <div class="border-2">
-      <my-canvas></my-canvas>
+      <my-canvas
+        v-if="allArt"
+        :key="allArt.artId"
+        :art="allArt"
+        :pixelSize="10"
+      ></my-canvas>
     </div>
   </div>
   <div class="border-solid border-1">
@@ -13,7 +18,14 @@ import Art from "@/entities/Art";
 import MyCanvas from "@/components/MyCanvas/MyCanvas.vue";
 import { ref, onMounted } from "vue";
 import Button from "primevue/button";
+import ArtAccessService from "../utils/ArtAccessService";
+
+const allArt = ref<Art | null>(null);
 
 const MyImg = ref<Art>();
-onMounted(() => {});
+onMounted(() => {
+  ArtAccessService.getArtById(1).then(
+    (promise) => (allArt.value = promise as Art),
+  );
+});
 </script>
