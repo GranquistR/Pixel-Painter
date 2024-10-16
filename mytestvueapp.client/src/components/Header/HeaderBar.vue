b1782d5b9fd0ecb5c24c6ec5de8ed94901d12357
Alexander Such <sucha3162@my.uwstout.edu>
2024-10-16 18:13:56 -0500

    Added login button into the modal, created redirect link, and added function to parse the modified redirect URI upon trusting the login.

--- mytestvueapp.client/src/components/Header/HeaderBar.vue
+++ mytestvueapp.client/src/components/Header/HeaderBar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink } from "vue-router";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import Dialog from "primevue/dialog";
 import InputText from "primevue/inputtext";
 import Button from "primevue/button";
@@ -8,62 +8,76 @@ import Password from "primevue/password";
 import DarkModeSwitcher from "./DarkModeToggle.vue";
 import Avatar from "primevue/avatar";
 
-const visible = ref(false);
-const password = ref(null);
+
+    const visible = ref(false);
+
+    // CSRF Token to compare to Google's
+    /*const state = crypto.randomBytes(16).toString("hex");
+    localStorage.setItem("CSRFToken", state);*/
+
+    function redirectToLogin() {
+        const clientId = '1030066146136-ld3kl9j8k7mllto6camk0psucphudvm8.apps.googleusercontent.com';
+        const redirectURI = 'https://localhost:5173';
+        const scope = 'email profile'; // Scopes the login to make sure the users email, name, and profile picture is obtained
+        //const response = 'code';
+
+        // const loginURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectURI}&scope=${scope}state=${state}&response_type=code`;
+        const loginURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectURI}&scope=${scope}&response_type=code`;
+        window.location.href = loginURL;
+    }     
+
+    onMounted(() => {
+        const googleScript = document.createElement('script');
+        googleScript.src = 'https://apis.google.com/js/platform.js';
+        googleScript.async = true;
+        document.body.appendChild(googleScript);
+
+        const params = new URLSearchParams(window.location.search);
+        const code = params.get('code');
+        const state = params.get('state');
+
+        console.log('Code: ', code);
+        console.log('State: ', state);
+    });
 </script>
 
-<!--<script src="https://apis.google.com/js/platform.js" async defer></script>
-<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">-->
-
 <template>
-  <div>
-    <Dialog
-      v-model:visible="visible"
-      modal
-      header="Sign Into Profile"
-      :style="{ width: '25rem' }"
-      dismissableMask="false"
-      @hide="password = ''"
-    >
-      <span class="text-surface-500 dark:text-surface-400 block mb-8"
-        >Input your information.</span
-      >
-      <div class="flex items-center gap-4 mb-4">
-        <label for="username" class="font-semibold w-24">Username</label>
-        <InputText id="username" class="flex-auto" autocomplete="off" />
-      </div>
-      <div class="flex items-center gap-4 mb-8">
-        <label for="password" class="font-semibold w-24">Password</label>
-        <Password
-          v-model="password"
-          :feedback="false"
-          id="password"
-          class="flex-auto"
-          autocomplete="off"
-          toggleMask
-        />
-      </div>
-      <div class="flex justify-end gap-2">
-        <Button
-          type="button"
-          label="Cancel"
-          severity="secondary"
-          @click="
+    <div>
+        <Dialog v-model:visible="visible"
+                modal
+                header="Sign Into Profile"
+                :style="{ width: '25rem' }"
+                dismissableMask="false"
+                @hide="password = ''">
+            <span class="text-surface-500 dark:text-surface-400 block mb-8">Input your information.</span>
+            <div class="flex items-center gap-4 mb-4">
+                <label for="username" class="font-semibold w-24">Username</label>
+                <InputText id="username" class="flex-auto" autocomplete="off" />
+            </div>
+            <div class="flex items-center gap-4 mb-8">
+                <label for="password" class="font-semibold w-24">Password</label>
+                <Password v-model="password"
+                          :feedback="false"
+                          id="password"
+                          class="flex-auto"
+                          autocomplete="off"
+                          toggleMask />
+            </div>
+            <div class="flex justify-end gap-2">
+                <Button type="button"
+                        label="Cancel"
+                        severity="secondary"
+                        @click="
             visible = false;
             password = '';
-          "
-        ></Button>
-        <Button
-          type="button"
-          label="Log in"
-          @click="
-            visible = false;
-            password = '';
-          "
-        ></Button>
-      </div>
-    </Dialog>
-  </div>
+          "></Button>
+                <Button type="button"
+                        label="Log in with Google"
+                        @click="redirectToLogin">
+                </Button>
+            </div>
+        </Dialog>
+    </div>
 
   <div class="flex justify-content-between align-items-center px-6 py-4">
     <RouterLink class="router-link-unstyled" to="/">
