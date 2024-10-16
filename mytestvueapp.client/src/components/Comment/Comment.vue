9243ad33f6fd4f3b68fe77c542bc07f0c83cf33c
AngdroRyu <cookmaster22@yahoo.com>
2024-10-15 20:20:57 -0500

    same as previous

---
+++ mytestvueapp.client/src/components/Comment/Comment.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="w-auto">
+        <div class="inline-block w-auto h-auto px-6 py-1 underline" id="User">{{userTest}}</div>
+        <div class="inline-block w-auto h-auto" id="Comment">{{commentTest}}</div>
+        <div class="inline-block w-auto h-auto px-3" id="Timestamp ">{{timeTest}}</div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+    import type Art from "@/entities/Art";
+    import { onMounted, ref } from "vue";
+    let commentTest = "This is a test comment and should look correct on the screen";
+    let userTest = "TestUser";
+    let timeTest = new Date();
+
+    const props = defineProps<{
+        art: Art;
+        //need Query to grab comment based on ID
+    }>();
+
+   
+</script>
---
+++ mytestvueapp.client/src/views/ImageViewer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="p-8 justify-content-center flex w-full h-full align-items-center">
+    <div class="border-2">
+      <my-canvas
+        v-if="allArt"
+        :key="allArt.artId"
+        :art="allArt"
+        :pixelSize="10"
+      ></my-canvas>
+    </div>
+  </div>
+  <div class="flex justify-content-center align-items-center"></div>
+  <div class="border-solid border-1">
+    <Comment></Comment>
+  </div>
+</template>
+<script setup lang="ts">
+import Art from "@/entities/Art";
+import MyCanvas from "@/components/MyCanvas/MyCanvas.vue";
+import { ref, onMounted } from "vue";
+import Button from "primevue/button";
+import ArtAccessService from "../utils/ArtAccessService";
+import Comment from "@/components/Comment/Comment.vue";
+const allArt = ref<Art | null>(null);
+
+const MyImg = ref<Art>();
+onMounted(() => {
+  ArtAccessService.getArtById(1).then(
+    (promise) => (allArt.value = promise as Art),
+  );
+});
+</script>
