2736cd6636fc2291a88a62b3fdf0ea2ab44ebe73
JacobSletten <slettenj2628@my.uwstout.edu>
2024-10-15 19:49:05 -0500

    ArtAccessService.ts now returns Art objects with embedded PixelGrid for .getAllArt() and .getArtById()

--- mytestvueapp.client/src/views/GalleryView.vue
+++ mytestvueapp.client/src/views/GalleryView.vue
@@ -3,12 +3,16 @@ import { onMounted, ref } from "vue";
 import ArtCard from "@/components/Gallery/ArtCard.vue";
 import Art from "@/entities/Art";
 import ArtAccessService from "@/utils/ArtAccessService";
+import { PixelGrid } from "@/entities/PixelGrid";
 
 const allArt = ref<Art[] | null>(null);
+const artpiece = ref<Art | null>(null);
 
 onMounted(() => {
   ArtAccessService.getAllArt() // Get All Art
     .then((promise) => (allArt.value = promise as Art[]));
+    ArtAccessService.getArtById(1) // Get All Art
+    .then((promise) => (artpiece.value = promise as Art));
 });
 </script>
 
@@ -16,6 +20,11 @@ onMounted(() => {
   <div class="w-9 mx-auto my-0">
     <div class="shrink-limit flex flex-wrap">
       <ArtCard v-for="art in allArt" :key="art.artId" :art="art" />
+      <ArtCard v-if="artpiece" :key="artpiece.artId" :art="artpiece" />
+      <div v-for="art in allArt" :key="art.artId">{{ art.pixelGrid }}</div>
+      <!-- {{ allArt?[1].pixelGrid }} -->
+      {{ "ArtPiece" + artpiece?.pixelGrid }} 
+      
     </div>
   </div>
 </template>
