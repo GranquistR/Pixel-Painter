a5abf642493ba6f5dfabe91d819229497ba20e86
JacobSletten <slettenj2628@my.uwstout.edu>
2024-10-16 14:50:10 -0500

    Cleaned Up Testing stuff

--- mytestvueapp.client/src/views/GalleryView.vue
+++ mytestvueapp.client/src/views/GalleryView.vue
@@ -6,13 +6,10 @@ import ArtAccessService from "@/utils/ArtAccessService";
 import { PixelGrid } from "@/entities/PixelGrid";
 
 const allArt = ref<Art[] | null>(null);
-const artpiece = ref<Art | null>(null);
 
 onMounted(() => {
   ArtAccessService.getAllArt() // Get All Art
     .then((promise) => (allArt.value = promise as Art[]));
-    ArtAccessService.getArtById(1) // Get All Art
-    .then((promise) => (artpiece.value = promise as Art));
 });
 </script>
 
@@ -20,11 +17,6 @@ onMounted(() => {
   <div class="w-9 mx-auto my-0">
     <div class="shrink-limit flex flex-wrap">
       <ArtCard v-for="art in allArt" :key="art.artId" :art="art" />
-      <ArtCard v-if="artpiece" :key="artpiece.artId" :art="artpiece" />
-      <div v-for="art in allArt" :key="art.artId">{{ art.pixelGrid }}</div>
-      <!-- {{ allArt?[1].pixelGrid }} -->
-      {{ "ArtPiece" + artpiece?.pixelGrid }} 
-      
     </div>
   </div>
 </template>
