129182888921d1efff44a4b26857a450d700f8b6
JohnNStout <100367310+JohnNStout@users.noreply.github.com>
2024-10-18 15:17:12 -0500

    set art background color

--- mytestvueapp.client/src/views/PromptPainter.vue
+++ mytestvueapp.client/src/views/PromptPainter.vue
@@ -1,22 +1,30 @@
 <template>
-<div class="flex justify-content-between align-items-center  px-6 py-4">
-
-    <h1>Select color: </h1>
+<FloatingCard position="center" header="Select Background Color" width="17rem">
+  <div class="flex justify-content-between align-items-center  px-6 py-4">
+  
     <ColorPicker class="m-1" v-model="backgroundColor" id="backgroundColorPick" format="hex" @change="updateLocalStorage()"></ColorPicker>
     
-    <RouterLink class="p-2" to="/paint"
+    
+    
+  
+  </div>
+</FloatingCard>
+<div  id="startPainting">
+<RouterLink class="p-2" to="/paint"
         ><Button rounded label="Start Painting" icon="pi pi-pencil"
       /></RouterLink>
-    
-</div>
+    </div>
 </template>
 <script setup lang="ts">
+
 import { RouterLink } from "vue-router";
 import Button from "primevue/button";
 import ColorPicker from "primevue/colorpicker";
+import FloatingCard from "./../components/PainterUi/FloatingCard.vue";
 
+localStorage.setItem('backgroundColor', '#ffffff');
 
-const backgroundColor = defineModel<string>({ default: "#777777" });
+const backgroundColor = defineModel<string>({ default: "#ffffff" });
 function updateLocalStorage(){
   localStorage.setItem('backgroundColor', backgroundColor.value);
 }
@@ -26,5 +34,13 @@ function updateLocalStorage(){
   width: 7rem !important;
   height: 2rem !important;
   border: solid 1px !important;
+  margin: 0 auto;
+}
+#startPainting{
+  position: fixed;
+  left: 50%;
+  bottom: 20px;
+  transform: translate(-50%, -50%);
+  margin: 0 auto;
 }
 </style>
