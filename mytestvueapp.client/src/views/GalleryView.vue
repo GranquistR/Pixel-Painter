8a916b8a507e2e2c3c772f5a7da1876ae7987804
Riley Dummer <dummerr2357@my.uwstout.edu>
2024-10-23 14:58:24 -0500

    Created search bar to filter art pieces by art name

--- mytestvueapp.client/src/views/GalleryView.vue
+++ mytestvueapp.client/src/views/GalleryView.vue
@@ -1,22 +1,42 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import ArtCard from "@/components/Gallery/ArtCard.vue";
 import Art from "@/entities/Art";
 import ArtAccessService from "@/utils/ArtAccessService";
 import { PixelGrid } from "@/entities/PixelGrid";
+import type { RefSymbol } from "@vue/reactivity";
 
 const allArt = ref<Art[] | null>(null);
+const displayArt = ref<Art[] | null>(null);
+const search = ref("")
+
 
 onMounted(() => {
   ArtAccessService.getAllArt() // Get All Art
-    .then((promise) => (allArt.value = promise as Art[]));
+    .then((promise) => (
+      allArt.value = promise as Art[],
+      displayArt.value = promise as Art[]
+    ));
 });
+
+
+watch(search, () =>{
+  if (allArt.value){
+    displayArt.value = allArt.value.filter(Art => Art.artName.toLowerCase().includes(search.value.toLowerCase()))
+  }
+})
+
 </script>
 
 <template>
   <div class="w-9 mx-auto my-0">
+    <header>
+      <h1 class="flex align-items-center gap-3">Search for Art
+      <input class="mt-2 " v-model.trim="search" type="text" placeholder="Search..." size="30" width="100%">
+      </h1>
+    </header>
     <div class="shrink-limit flex flex-wrap">
-      <ArtCard v-for="art in allArt" :key="art.artId" :art="art" />
+      <ArtCard v-for="art in displayArt" :key="art.artId" :art="art" />
     </div>
   </div>
 </template>
