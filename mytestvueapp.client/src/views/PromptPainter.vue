044d630dee3fc8c0fa948228379803aa238df0c0
JohnNStout <100367310+JohnNStout@users.noreply.github.com>
2024-10-23 14:16:32 -0500

    Set up framework for title and description input

--- mytestvueapp.client/src/views/PromptPainter.vue
+++ mytestvueapp.client/src/views/PromptPainter.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="absolute bottom-50 bg-primary flex align-items-center justify-content-center w-full h-10rem">
 
+    <Card >
+    <template #title>Enter Title And Description Of Art</template>
+    <template #content>
+      <InputText class="mt-2 w-full" type="text" v-model="title" placeholder="Title"/>
+
+      <Textarea class="mt-2 w-full" name="textArea" placeholder="Description" v-model="description"/>
+
+    </template>
+</Card>
+
   <Card >
     <template #title>Select Background Color</template>
     <template #content>
@@ -22,10 +32,14 @@ import { RouterLink } from "vue-router";
 import Button from "primevue/button";
 import ColorPicker from "primevue/colorpicker";
 import Card from 'primevue/card';
+import Textarea from "primevue/textarea";
+import InputText from 'primevue/inputtext';
+
 
 
 localStorage.setItem('backgroundColor', '#ffffff');
-
+const title = defineModel<string>('title');
+const description = defineModel<string>('description',{default:""});
 const backgroundColor = defineModel<string>({ default: "#ffffff" });
 function updateLocalStorage(){
   localStorage.setItem('backgroundColor', backgroundColor.value);
