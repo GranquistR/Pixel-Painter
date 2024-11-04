76b1c0026fc0ac1feee80c6595d4b5f4427e0222
Riley Dummer <dummerr2357@my.uwstout.edu>
2024-11-04 16:08:15 -0600

    Added Comments section title, text box for adding comments and styled comments to look more appealing

--- mytestvueapp.client/src/components/Comment/CommentOnArt.vue
+++ mytestvueapp.client/src/components/Comment/CommentOnArt.vue
@@ -1,18 +1,32 @@
 <template>
+<!--   
   <div class="w-full flex h-auto p-1 flex-nowrap">
     <div
-      class="inline-block w-2 h-auto px-6 py-1 underline text-lg"
+      class="inline-block w-2 h-auto px-7 py-2 underline text-lg"
       id="User"
     >
       {{ props.comment.artistName }}
     </div>
-    <div class="inline-block text-left w-8 h-auto white-space-normal" id="Comment">
-      {{ props.comment.commentContent }}
-    </div>
-    <div class="inline-block w-2 h-auto px-3" id="Timestamp ">
+    <div class="w-2 h-auto text-align-left py-2" id="Timestamp ">
       {{ props.comment.commentTime }}
     </div>
+    
   </div>
+   <div class="inline-block text-left w-8 h-auto px-8 white-space-pre mt-1" id="Comment">
+      {{ props.comment.commentContent }}
+    </div> -->
+    <div class="m-2 px-6">
+
+      <div class="flex gap-3">
+        
+        <span style="font-weight: bold;">{{ comment.artistName }}</span>
+        <span>{{ comment.commentTime }}</span>
+      </div>
+      <div class="mb-4 ml-2">
+        
+        <span >{{ comment.commentContent }}</span>
+      </div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -22,5 +36,5 @@ import { onMounted, ref } from "vue";
 
 const props = defineProps<{
   comment: Comment;
-}>();
+}>()
 </script>
--- mytestvueapp.client/src/views/ImageViewer.vue
+++ mytestvueapp.client/src/views/ImageViewer.vue
@@ -9,12 +9,8 @@
       ></my-canvas>
     </div>
   </div>
-  <h1 class="px-4">Comments Section</h1>
   <div class="flex justify-content-center align-items-center"></div>
   <div class="border-solid border-1"></div>
-  <h2 class="mb-2 ml-6">Add your own comment</h2>
-  <textarea class="ml-6" placeholder="Write your comment..."></textarea>
-  <div><button class="mt-2 ml-6">Post Comment</button></div>
   <CommentOnArt
     v-for="Comment in allComments"
     :key="Comment.commentId"
@@ -28,21 +24,16 @@ import { ref, onMounted } from "vue";
 import Comment from "@/entities/Comment";
 import CommentOnArt from "@/components/Comment/CommentOnArt.vue";
 import ArtAccessService from "../services/ArtAccessService";
-import { useRoute } from "vue-router";
-
-const route = useRoute();
 
 const allArt = ref<Art | null>(null);
 const allComments = ref<Comment[] | null>(null);
 
 onMounted(() => {
-  const id = Number(route.params.id);
-
-  ArtAccessService.getArtById(id).then((promise: Art) => {
-    allArt.value = promise as Art;
-  });
-  ArtAccessService.getCommentsById(id).then((promise: Comment[]) => {
-    allComments.value = promise;
-  });
+  ArtAccessService.getArtById(2).then(
+    (promise) => (allArt.value = promise as Art)
+  );
+  ArtAccessService.getCommentsById(2).then(
+    (promise) => (allComments.value = promise as Comment[])
+  );
 });
 </script>
