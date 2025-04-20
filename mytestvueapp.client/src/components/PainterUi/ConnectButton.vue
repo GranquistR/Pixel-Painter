ae8b18c2e5dc3cc1c3b6fdbf8cd024ed21c4b31b
JacobSletten <slettenj2628@my.uwstout.edu>
2025-04-19 22:47:52 -0500

    Added back text describing disabled features and deleted unused code

--- mytestvueapp.client/src/components/PainterUi/ConnectButton.vue
+++ mytestvueapp.client/src/components/PainterUi/ConnectButton.vue
@@ -8,13 +8,13 @@
     />
 
     <Dialog v-model:visible="visible" modal :style="{width:'25rem'}" :show-header="false">
-            <div class="inline-flex items-center justify-content-between w-full">
-                <h1 class="m-0 align-self-center text-center">Connect to a group?</h1>
-                <Button class="align-self-center mt-2" icon="pi pi-times" severity="secondary" text rounded @click="visible = false"/>
-            </div>
-        <!--
-        <h4 style="margin:0; font-size: 12px;">This will disable: Adding/Removing Layers, Gravity functions</h4>
-        -->
+        <div class="inline-flex items-center justify-content-between w-full">
+            <h1 class="m-0 align-self-center text-center">Connect to a group?</h1>
+            <Button class="align-self-center mt-2" icon="pi pi-times" severity="secondary" text rounded @click="visible = false"/>
+        </div>
+
+        <p class="m-0 text-xs">This will disable: Adding/Removing Layers, Gravity functions</p>
+
         <Tabs value="0">
             <TabList>
                 <Tab value="0" @click="tab = 0">Join Group</Tab>
@@ -23,13 +23,6 @@
             <TabPanels class="pb-0 px-0 pt-1">
                 <TabPanel value="0">
                     <div>
-                        <!-- <h4 style="margin-top: 0px; margin-bottom: 0px"> Group Name | Active Member Count</h4>
-                        <ScrollPanel style="width: 100; height: 100px; margin-bottom: 10px; margin-top: 0px;">
-                        <div v-for="group in groups" :key="group.groupName" class="inline-flex items-center justify-center">
-                            {{`Group Name: ${group.groupName} Member Count: ${group.memberCount}`}}
-                        </div>
-                        </ScrollPanel> -->
-
                         <DataTable v-if="groups.length > 0" :value="groups" scrollable scroll-height="200px">
                             <Column field="groupName" header="Name" class="w-6 p-1 h-1" ></Column>
                             <Column field="memberCount" header="Count" class="w-2 p-1 h-1"></Column>
@@ -74,11 +67,11 @@
 </template>
 
 <script setup lang="ts">
-    import { onMounted, ref, watch } from "vue";
+    import { ref, watch } from "vue";
     import Button from "primevue/button";
     import Dialog from "primevue/dialog";
     import InputText from "primevue/inputtext";
-    import { ScrollPanel, DataTable, Column } from "primevue";
+    import { DataTable, Column } from "primevue";
     import SocketService from "@/services/SocketService";
     import GroupAdvert from "@/entities/GroupAdvert";
     import {Tabs, TabList, Tab, TabPanels, TabPanel } from "primevue";
@@ -121,5 +114,6 @@
             .then((data) => {
                 groups.value = data;
             });
+        tab.value = 0;
     });
 </script>
